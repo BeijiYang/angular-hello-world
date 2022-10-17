bcda14d605699581f971c15a945661dd04564624
Yujie Wang <langyuyang@outlook.com>
2022-10-17 19:41:56 +0800

    add recipe list

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
 
+export class RecipeListComponent implements OnInit {
+  recipes: Array<Recipe> = [
+    new Recipe('A Test Recipe', 'This is a test recipe', 'https://img0.baidu.com/it/u=2500107091,3723826537&fm=253&fmt=auto&app=138&f=PNG?w=586&h=500'),
+    new Recipe('A Test Recipe', 'This is also a test recipe', 'https://img2.baidu.com/it/u=900444370,3315059522&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=800')
+
+  ]
   constructor() { }
 
   ngOnInit(): void {
