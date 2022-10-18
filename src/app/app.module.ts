d89919f9020a328fe24b3d7c8df46db8a1d71ee7
Yujie Wang <langyuyang@outlook.com>
2022-10-18 23:31:39 +0800

    make adding item in the shopping list works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ViewChild, ElementRef } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() onAddIngredient = new EventEmitter<Ingredient>();
+  @ViewChild('nameInput', { static: true }) nameInput: ElementRef;
+  @ViewChild('amountInput', { static: true }) amountInput: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleAdd() {
+    const name = this.nameInput.nativeElement.value;
+    const amount = this.amountInput.nativeElement.value;
+    const ingredient = new Ingredient(name, amount);
+    this.onAddIngredient.emit(ingredient);
+
+    this.nameInput.nativeElement.value = '';
+    this.amountInput.nativeElement.value = '';
+  }
 }
