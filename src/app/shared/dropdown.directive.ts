0c19e620c7955ce7bc8c8889e852435d3bf5eab9
Yujie Wang <langyuyang@outlook.com>
2022-10-19 19:03:02 +0800

    improve dropdown directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener } from "@angular/core";
 
 @Directive({
   selector: '[appDropdown]'
@@ -7,7 +7,18 @@ import { Directive, HostBinding, HostListener } from "@angular/core";
 export class DropdownDirective {
   @HostBinding('class.open')  isOpen = false;
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  // the prvious version
+  // @HostListener('click') toggleOpen() {
+  //   this.isOpen = !this.isOpen;
+  // }
+
+  @HostListener('document:click', ['event']) toggleOpen() {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) // clicking inside of the current element?
+                  ? !this.isOpen // if true, like the prvious version, clicking the element to toggle the "open" in classes
+                  : false; // if not, clicking outside of the current element can close the dropdown
+  }
+
+  constructor(private elRef: ElementRef) {
+
   }
 }
