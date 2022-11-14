38992d2bb9a61af10c4b660177789b50a22edf37
Yujie Wang <langyuyang@outlook.com>
2022-11-14 19:50:45 +0800

    set up & use app route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    // empty path, it'll be loaded on the first time
+    path: '',
+    redirectTo: '/recipes',
+    pathMatch: 'full'
+
+  },
+  {
+    path: 'recipes',
+    component: RecipeComponent
+  },
+  {
+    path: 'shopping-list',
+    component: ShoppingListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
